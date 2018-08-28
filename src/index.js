b2f74318ccb78443018970d0f5be7f15b45c6983
Jessica Patterson <jessica@theredsunflower.com>
2018-08-27 21:04:00 -0400

    app functioning properly, sw compiled using webpack, working indexeddb database

--- src/index.js
+++ src/index.js
@@ -1,11 +1,79 @@
-import _ from 'lodash';
+import idb from 'idb';
 
-function component() {
-  let element = document.createElement('div');
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html', 
+        '/restaurant.html?id={x}'
+      ]);
+    })
+  );
+});
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response)
+    {
+      return response || fetch(event.request);
+    })
+  );
+});
 
-  return element;
-}
+var dbPromise = idb.open('people', 1, function(upgradeDb) {
+	if (!upgradeDb.objectStoreNames.contains('people')) {
+		upgradeDb.createObjectStore('people', {keyPath:'id'});
+	}
+});
 
-document.body.appendChild(component());
+dbPromise.then(function(db) {
+	var transaction = db.transaction(['people'], 'readwrite');
+	var store = transaction.objectStore('people');
+	var item = {
+        "name": "Mission Chinese Food",
+        "neighborhood": "Manhattan",
+        "photograph": "1",
+        "address": "171 E Broadway, New York, NY 10002",
+        "latlng": {
+            "lat": 40.713829,
+            "lng": -73.989667
+        },
+        "cuisine_type": "Asian",
+        "operating_hours": {
+            "Monday": "5:30 pm - 11:00 pm",
+            "Tuesday": "5:30 pm - 11:00 pm",
+            "Wednesday": "5:30 pm - 11:00 pm",
+            "Thursday": "5:30 pm - 11:00 pm",
+            "Friday": "5:30 pm - 11:00 pm",
+            "Saturday": "12:00 pm - 4:00 pm, 5:30 pm - 12:00 am",
+            "Sunday": "12:00 pm - 4:00 pm, 5:30 pm - 11:00 pm"
+        },
+        "reviews": [
+            {
+                "name": "Steve",
+                "date": "October 26, 2016",
+                "rating": 4,
+                "comments": "Mission Chinese Food has grown up from its scrappy Orchard Street days into a big, two story restaurant equipped with a pizza oven, a prime rib cart, and a much broader menu. Yes, it still has all the hits â€” the kung pao pastrami, the thrice cooked bacon â€”but chef/proprietor Danny Bowien and executive chef Angela Dimayuga have also added a raw bar, two generous family-style set menus, and showstoppers like duck baked in clay. And you can still get a lot of food without breaking the bank."
+            },
+            {
+                "name": "Morgan",
+                "date": "October 26, 2016",
+                "rating": "4",
+                "comments": "This place is a blast. Must orders: GREEN TEA NOODS, sounds gross (to me at least) but these were incredible!, Kung pao pastrami (but you already knew that), beef tartare was a fun appetizer that we decided to try, the spicy ma po tofu SUPER spicy but delicous, egg rolls and scallion pancake i could have passed on... I wish we would have gone with a larger group, so much more I would have liked to try!"
+            },
+            {
+                "name": "Jason",
+                "date": "October 26, 2016",
+                "rating": "3",
+                "comments": "I was VERY excited to come here after seeing and hearing so many good things about this place. Having read much, I knew going into it that it was not going to be authentic Chinese. The place was edgy, had a punk rock throwback attitude, and generally delivered the desired atmosphere. Things went downhill from there though. The food was okay at best and the best qualities were easily overshadowed by what I believe to be poor decisions by the kitchen staff."
+            }
+        ],
+        "createdAt": "2017-07-25T02:26:54.985Z",
+        "updatedAt": "2017-07-25T02:26:54.985Z",
+        "id": 1
+    };
+	store.add(item);
+	return transaction.complete;
+});
