d790af4aa771719ac4b59a21de52af0e36e4195c
Jessica Patterson <jessica@theredsunflower.com>
2018-09-02 18:35:39 -0400

    using indexed db, app works offline

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "sw.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'sw.js',
+    path: path.resolve(__dirname, 'dist')
   },
-  "module": {
-        "rules": [
+    devServer: {
+        contentBase: './dist'
+    },
+  module: {
+        rules: [
             {
-                "test": /\.js$/,
-                "exclude": /node_modules/,
-                "use": {
-                    "loader": "babel-loader",
-                    "options": {
-                        "presets": [
-                            "env"
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    'loader': 'babel-loader',
+                    'options': {
+                        'presets': [
+                            'env'
                         ]
                     }
                 }
